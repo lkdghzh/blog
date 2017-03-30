f0bbd6917522aea42bd278ebf73d3822b10593cc
like <416960428@qq.com>
2017-03-30 11:38:26 +0800

    vscode

--- requirejsProject/js/app/entry.js
+++ requirejsProject/js/app/entry.js
@@ -1,10 +1,17 @@
 // console.log(1)
-requirejs.config({
-    baseUrl: '',//js/lib
+// requirejs.config({
+//     baseUrl: 'js/lib',
+//     path:{
+//
+//     }
+// })
+debugger
+require.config({
+    baseUrl: 'js/lib',
     path:{
 
     }
 })
-requirejs(['js/index/b.js'],function (b) {
+requirejs(['js/index/b'],function (b) {
     console.log(b.b)
 })
