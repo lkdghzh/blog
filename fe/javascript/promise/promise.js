54b882d2ceab68f1683911e912a3f9af13e6a62c
lkdghzh <likesite@foxmail.com>
2018-06-28 17:24:00 +0800

    promise .

--- fe/javascript/promise/promise.js
+++ fe/javascript/promise/promise.js
@@ -1,52 +1,56 @@
-let fs = require('fs');
-let path = require('path');
 /**
  * 原生是通过new 的形式创建，所以定一个类
  */
-class P {
+class Promise {
     /**
-     * promise：
-     * 1：保持状态
-     * 2：存多个回调函数
-     * promise.then(fn1,fn2)会被调用多次，使用resovle，reject标记为成功、失败
-     * 将多次的成功fn1函数、失败fn2函数，使用数组存一下
+     * promise
      * @param {*} fn  //一个函数（异步、同步）
      */
     constructor(fn) {
+        if (typeof fn !== 'function') {
+            throw new TypeError("Promise Constructor's argument is not a function")
+        }
+        //1：保持状态
         this.__state = 'pending'
+        //当前promise执行后的返回值。为下次then使用
+        this._value = undefined
 
-        this.__onResolveFns=[]
-        this.__onRejectFns=[]
+        // 2：存多个回调函数
+        // then(fn1,fn2)会被调用多次，使用resovle，reject标记为成功、失败
+        // 将多次的成功fn1函数、失败fn2函数，使用数组存一下
+        this.__onResolveFns = []
+        this.__onRejectFns = []
 
         //fn的函数参数
         //操作成功，调用resolve并传入value
         //操作失败，调用reject并传入info
-        const __resolve = function (value) {
-
+        const resolve = function (value) {
+            this._value = value
+            this.__state = 'resolved'
         }
-        const __reject = function (info) {
-
+        const reject = function (info) {
+            this._value = info
+            this.__state = 'rejected'
         }
+
         //1：创建promise实例。
         //2：执行传入函数，
-        //执行的时候，可能会出错,把错误也用__reject返回出去
+        //执行的时候，可能会出错,把错误也用reject返回出去
         try {
-            fn(__resolve, __reject)
+            fn(resolve, reject)
         } catch (e) {
-            __reject(e)
+            reject(e)
         }
 
     }
     then(onFulfilled, onRejected) {
-
+        var fn = undefined
+        if (this.status === 'resolved') {
+            fn = onFulfilled(this._value)
+        }
+        if (this.status === 'rejected') {
+            fn = onRejected(this._value)
+        }
+        return new Promise(fn)
     }
 }
-const fileUrl = path.resolve(__dirname, './1.txt')
-console.log(fileUrl)
-
-var a = new P(function (__resolve, __reject) {
-    fs.readFile(fileUrl, 'utf-8', (err, data) => {
-        if (err) throw err;
-        console.log(data);
-    })
-})
--- fe/javascript/promise/rawPromise.js
+++ fe/javascript/promise/rawPromise.js
@@ -1,3 +1,27 @@
-var Promise=require('Promise')
-var p=new Promise()
-console.log(p)
+let fs = require('fs');
+let path = require('path');
+
+const fileUrl1 = path.resolve(__dirname, './1.txt')
+const fileUrl2 = path.resolve(__dirname, './2.txt')
+const fileUrl3 = path.resolve(__dirname, './3.txt')
+console.log(fileUrl1, fileUrl2)
+
+var a = new Promise(function (resolve, reject) {
+    fs.readFile(fileUrl1, 'utf-8', (err, txt1) => {
+        if (err) throw err;
+        console.log(txt1);
+        resolve(txt1)
+    })
+}).then(function (txt1) {
+    debugger
+    fs.readFile(fileUrl2, 'utf-8', (err, txt2) => {
+        if (err) throw err;
+        console.log(txt2);
+    })
+}).then(function (txt2) {
+    debugger
+    fs.readFile(fileUrl3, 'utf-8', (err, txt3) => {
+        if (err) throw err;
+        console.log(txt3);
+    })
+})
