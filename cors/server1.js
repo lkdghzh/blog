d1fc9b438c19a7a84938f9876f7261854c64d396
lkdghzh <likesite@foxmail.com>
2020-02-29 16:11:13 +0800

    feat: regular request demo

--- cors/server1.js
+++ cors/server1.js
@@ -2,13 +2,21 @@ const Koa = require('koa')
 const fs=require('fs')
 const path=require('path')
 
+const Router = require('koa-router')
+const router = new Router()
 const app = new Koa()
-app.use(async (ctx)=>{
+
+router.get('/', async ctx => {
   const ABPath=path.join(__dirname,'index.html')
   console.log(ABPath)
   ctx.type='html'
   ctx.body=fs.createReadStream(ABPath)
 })
+
+router.get('/nocors', async ctx => {
+  ctx.body = '/nocors'
+})
+app.use(router.routes())
 
 app.listen(3000)
 
